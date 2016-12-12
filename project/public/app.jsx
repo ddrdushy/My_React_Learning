d194dae1ccd0ee78dace061fe7b70bfedb4a7c14
ddrdushy <dushy2009@gmail.com>
2016-12-12 23:01:36 +0530

    componennt is created with es6 style

--- project/public/app.jsx
+++ project/public/app.jsx
@@ -1,8 +1,9 @@
 var Greeter=React.createClass({
-  render:function(){
+  render:()=>{
     return(
       <div>
         <h1>Hello React!</h1>
+        <p>Hello this is from react component</p>
       </div>
     );
   }
