5c2655583180f2456f5ec140e29de3d272cd462e
ddrdushy <dushy2009@gmail.com>
2016-12-13 22:27:17 +0530

    created nested components

--- project/public/app.jsx
+++ project/public/app.jsx
@@ -1,3 +1,25 @@
+var GreeterMessage=React.createClass({
+  render:function(){
+    return(
+      <div>
+        <h1>Hello h1!</h1>
+        <p>paragraph</p>
+      </div>
+    );
+  }
+});
+
+var FormComponent=React.createClass({
+  render:function(){
+    return(
+      <form>
+        <input type="text" ref="name"></input>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
 var Greeter=React.createClass({
   getDefaultProps:function(){
     return {
@@ -33,10 +55,14 @@ var Greeter=React.createClass({
         <h1>Hello {name}!</h1>
         <p>{message}</p>
 
+        <GreeterMessage/>
+
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"></input>
           <button>Set Name</button>
         </form>
+
+        <FormComponent/>
       </div>
     );
   }
